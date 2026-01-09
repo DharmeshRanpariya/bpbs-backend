c2c4628de93b84bee2f0b15829d16471465adbf7
Dharmesh Ranpariya <dharmeshranpariya@gmail.com>
2026-01-09 11:03:34 +0530

    Connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/school_db'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
